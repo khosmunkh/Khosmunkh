9eef7c842bed008cbd354895a6d73857c51b48da
khosmunkh <69497227+khosmunkh@users.noreply.github.com>
2020-10-23 14:19:27 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function Mendchilgee123(props){console.log(props)
+    return (
+        <div>
+            <h1>{props.ner}</h1>
+            <p>{props.nas}</p>
+            <p>{props.surchBgaEseh ? "unen":"choloo avsan"}</p>
+            <p>{props.dunguud[0]}</p>
+            <p>{props.dunguud[1]}</p>
+            <p>{props.dunguud[2]}</p>
+           
+            {props.dunguud.map(
+                (el,index) => 
+                <p key={index}>{el}</p>
+                )
+            }
+            <p>{props.myObj.mergejil}</p>
+            <p>{props.myObj.index}</p>
+        </div>
+    );
+}
+ReactDOM.render(
+    <Mendchilgee123 ner={"Hello dorj"}
+    nas={20}
+    surchBgaEseh={true}
+    dunguud={[80,"dalan yos", true]}
+    myObj={ {mergejil: 'SW' , index:'DS2015877'} }/>,
+    document.getElementById('root')
+);
